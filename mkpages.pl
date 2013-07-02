bf0a6c423ac543603be1596c42d01c3174b3f1ce
Matt Sergeant <helpme+github@gmail.com>
2013-07-02 19:43:27 -0400

    Brought docs up to date

--- mkpages.pl
+++ mkpages.pl
@@ -3,6 +3,23 @@
 my @files = `find ../Haraka-publish -type f -name \*.md`;
 chomp(@files);
 
+sub sort_order {
+    my ($filea, $fileb) = @_;
+    if ($filea =~ /README/) {
+        return -1;
+    }
+    if ($fileb =~ /README/) {
+        return +1;
+    }
+    if ($filea =~ /\/tutorial/i) {
+        return -1;
+    }
+    if ($filea =~ /\/plugins\//) {
+        return +1;
+    }
+    return 0;
+}
+
 sub output {
     my $in = shift;
     $in =~ s/.*\/docs/manual/;
@@ -29,8 +46,10 @@ my $wrapper = `cat template.html`;
 
 my $chapter_out = '';
 my $plugins_sent = 0;
+my $tutorials_sent = 0;
+my $core_sent = 0;
 
-for my $file (sort { dirname($a) cmp dirname($b) } @files) {
+for my $file (sort { sort_order($a, $b) } @files) {
     my $out = output($file);
     print "Processing $file => $out\n";
     system("mkdir", "-p", dirname($out)) unless $file =~ /README/;
@@ -40,7 +59,7 @@ for my $file (sort { dirname($a) cmp dirname($b) } @files) {
 
     my ($title) = ($output =~ /<h1>([^<]*)/);
     $title ||= "Haraka";
-    $title .= " plugin" if $out =~ /plugin/;
+    # $title .= " plugin" if $out =~ /plugin/;
 
     my $template = $wrapper;
     $template =~ s/<\%=\s*title\s*\%>/$title/g;
@@ -52,7 +71,17 @@ for my $file (sort { dirname($a) cmp dirname($b) } @files) {
     if (!$plugins_sent && $out =~ /plugin/) {
         $plugins_sent++;
         $chapter_out .= "<hr>\n";
+        $chapter_out .= "<h2>Plugins</h2>\n";
     }
+    elsif (!$tutorials_sent && $out =~ /tutorial/i) {
+        $tutorials_sent++;
+        $chapter_out .= "<hr>\n<h2>Tutorials</h2>\n";
+    }
+    elsif ($out !~ /(tutorial|plugin)/i && !$core_sent) {
+        $core_sent++;
+        $chapter_out .= "<hr>\n<h2>Core</h2>\n";
+    }
+
     $chapter_out .= "<li><a href='/$out' target=\"content\">$title</a></li>\n";
 }
 
